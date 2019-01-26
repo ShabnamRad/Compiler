ddb9630910847568c142864416b403a4a3dd42b6
Shabnam Ghasemirad <shabnam_996@yahoo.com>
2019-01-26 21:16:32 +0330

    initialization for diagrams

--- src/Parser/SyntaxAnalyzer.java
+++ src/Parser/SyntaxAnalyzer.java
@@ -3,61 +3,47 @@ package Parser;
 import Scanner.LexicalAnalyzer;
 import Scanner.Token;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class SyntaxAnalyzer {
-    private ArrayList<Diagram> transitionDigrams;
     private LexicalAnalyzer lexicalAnalyzer;
     private Grammar grammar = Grammar.getInstance();
 
     public SyntaxAnalyzer(LexicalAnalyzer lexicalAnalyzer) {
         this.lexicalAnalyzer = lexicalAnalyzer;
-        transitionDigrams = new ArrayList<>();
-        Diagram program = new Diagram("program");
-        Diagram declaration_list = new Diagram("declaration-list");
-        Diagram params = new Diagram("params");
-        Diagram statement_list = new Diagram("statement-list");
-        Diagram statement = new Diagram("statement");
-        Diagram expression = new Diagram("expression");
-        Diagram var = new Diagram("var");
-        Diagram term = new Diagram("term");
-        Diagram factor = new Diagram("factor");
-        transitionDigrams = new ArrayList<>(Arrays.asList(program, declaration_list, params, statement_list, statement,
-                expression, var, term, factor));
 
-        //Constructing Diagrams
-        State state = new State(true);
-        program.addState(state);
-        Map<Edge, State> children = new HashMap<>();
-        children.put(new Edge("\uFFFF"), state);
-        state = new State(children);
-        program.addState(state);
-        children = new HashMap<>();
-        children.put(new Edge(declaration_list), state);
-        state = new State(children);
-        program.setStartState(state);
 
-        state = new State(true);
-        declaration_list.addState(state);
-        children = new HashMap<>();
-        children.put(new Edge("\\eps"), state);
-        state = new State(children);
-        declaration_list.setStartState(state);
-        children = new HashMap<>();
-        children.put(new Edge("\\eps"), state);
-        state = new State(children);
-        declaration_list.addState(state);
-        children = new HashMap<>();
-        children.put(new Edge(";"), state);
-        state = new State(children);
-        declaration_list.addState(state);
-        children.put(new Edge(";"), declaration_list.getStates().get(2));
-        state = new State(children);
-        declaration_list.addState(state);
     }
+
+    private boolean call(String functionName) {
+        switch (functionName) {
+            case "program":
+                return program();
+            case "declaration-list":
+                return declaration_list();
+            case "params":
+                return params();
+            case "statement":
+                return statement();
+            case "expression":
+                return expression();
+            case "term":
+                return term();
+            case "factor":
+                return factor();
+            case "statement-list":
+                return statement_list();
+            default:
+                    return false;
+        }
+    }
+
+    private boolean program(){return true;}
+    private boolean declaration_list(){return true;}
+    private boolean params(){return true;}
+    private boolean statement(){return true;}
+    private boolean expression(){return true;}
+    private boolean term(){return true;}
+    private boolean factor(){return true;}
+    private boolean statement_list(){return true;}
 
     private boolean traverse(Diagram diagram) {
         State curr = diagram.getStartState();
